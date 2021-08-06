1e24da9bd05a9cb97db1eef98530e16481342273
Mihaja15 <mihajaranadison@outlook.fr>
2021-08-06 13:15:37 +0300

    use api

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ function urlConf(){
     if(process.env.NODE_ENV ==='development')
         return 'http://localhost:8080';
     else
-        return 'http://developer.meddoc.mg';
+        return 'https://api.meddoc.mg';
 }
 
 export default urlConf;
